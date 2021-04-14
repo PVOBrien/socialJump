4dad7922a612e1f19e012ff7ec2022bbd0058d14
Paul OBrien <PVOVideo@outlook.com>
2021-04-13 20:41:18 -0700

    save point, can't get something out of post route

--- src/main/java/com/pvobrien/socialJump/SecurityConfig.java
+++ src/main/java/com/pvobrien/socialJump/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.pvobrien.socialJump.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +32,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll().
+                anyRequest().authenticated().
+                and().
+                addFilterBefore(new LoginFilter("/login",
+                        authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new AuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        config.applyPermitDefaultValues();
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 //    @Override
---
+++ src/main/java/com/pvobrien/socialJump/service/AuthenticationService.java
@@ -0,0 +1,46 @@
+package com.pvobrien.socialJump.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+import static java.util.Collections.emptyList;
+
+public class AuthenticationService {
+    static final long EXPIRATIONTIME = 864_00_00; // 1 day in milliseconds, okay.
+    static final String SIGNINGKEY = "SecretKey"; // ???
+    static final String PREFIX = "Bearer";
+
+    // to add token to Auth header...
+    static public void addToken(HttpServletResponse res, String username) {
+        String JwtToken = Jwts.builder().setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .signWith(SignatureAlgorithm.HS256, SIGNINGKEY)
+                .compact(); // todo: what?
+        res.addHeader("Authorization", PREFIX + " " + JwtToken);
+        res.addHeader("Access-Control-Expose-Headers", "Authorization");
+    }
+
+    // get token from auth header... (why do this, we just created it?)
+    static public Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token != null) {
+            String user = Jwts.parser()
+                    .setSigningKey(SIGNINGKEY)
+                    .parseClaimsJws(token.replace(PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, emptyList());
+            }
+
+        }
+        return null;
+    }
+}
